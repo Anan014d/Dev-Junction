17e40ff13b866853079524ca5b95832cd288e31a
Anan014d <22bec014@iiitdmj.ac.in>
2025-02-12 00:07:41 +0530

    name added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -27,7 +27,7 @@ export const Navbar = () => {
           <div className="flex items-center">
             <Link to="/" className="flex items-center space-x-2">
               <span className="text-xl font-bold text-gray-900 dark:text-white">
-                SYNERGY
+                Dev Junction
               </span>
             </Link>
           </div>
